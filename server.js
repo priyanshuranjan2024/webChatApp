32ece268a8f3157c202df1ef16918ac50e405773
Priyanshu Ranjan <priyanshuranjan2020@gmail.com>
2024-04-30 21:33:09 +0530

    completed the styling and the frontend part

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 2020 });
+const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (client) => {
   client.on("message", (message, isBinary) => {
