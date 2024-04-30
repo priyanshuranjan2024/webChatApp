be27c826ed22e4d3d98c8c6450581dee8520abb3
Priyanshu Ranjan <priyanshuranjan2020@gmail.com>
2024-04-30 20:15:19 +0530

    completed the backend

---
+++ server.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 2020 });
+
+wss.on("connection", (client) => {
+  client.on("message", (message, isBinary) => {
+    //excluding the current client from the array of the connected clients
+    [...wss.clients]
+      .filter((c) => c !== client)
+      .forEach((c) => c.send(isBinary ? message.toString() : message));
+  });
+});
